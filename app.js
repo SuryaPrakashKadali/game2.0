ed04044d93f96373cef77b7cf42d5bfcb11d2c79
SuryaPrakashKadali <41120816+SuryaPrakashKadali@users.noreply.github.com>
2019-05-28 17:32:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
-        // Add CURRENT score to GLOBAL score
+        
         scores[activePlayer] += roundScore;
 
         // Update the UI
